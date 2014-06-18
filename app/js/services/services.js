a79ab5af5bf7187310f89f5a7cd35a3393131a66
Vlad Firoiu <vladfi2@gmail.com>
2014-06-17 20:30:01 -0400

    random data function

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -13,24 +13,24 @@ myApp.factory("VentureService", function($http, $interval, parser) {
     //Sends commands to the server and then gets directives
     //NOTE: not hooked up yet so currently the code comments out the ajax request
     vs.sendCmd = function(cmd) {
-        this.commands.push(cmd); //done so we can implement a feature to use up arrow for previous command
-        //$http.post("", cmd)
-          //  .success(function(data) {
-                    var data = {}; //temporary will remove
+        var that = this;
+        that.commands.push(cmd); //done so we can implement a feature to use up arrow for previous command
+        $http.post("http://127.0.0.1:8082/assume", ['y', '(normal 0 1)'])
+            .success(function(data) {
                     var complete = parser.api(data);
-                    complete = {command: cmd, id:'#', value:'some value', error:'', success:true};
-                    this.apiReturns.push(complete);
-                    if(!this.continuous) {
-                        this.getDirectives();
+                    complete = {command: cmd, id:'#', value:data.value, error:'', success:true};
+                    that.apiReturns.push(complete);
+                    if(!that.continuous) {
+                        that.getDirectives();
                     }
-            //})
-//            .error(function() {
-//                console.log("Did not reach server");
-//            });
+            })
+            .error(function() {
+                console.log("Did not reach server");
+            });
     };
 
     // This method is dedicated to getting list directives, the method will get all directives, identify the 'assumes' so that we
-    // can act on those parameters for the x and y choices in the chart. Ajax request commented out until we hook into server
+    // can act on those parameters for the x and y choices in the chart.
     vs.getDirectives = function(that) {
         // update the set of assumes
         var handleAssumes = function(data) {
@@ -56,21 +56,29 @@ myApp.factory("VentureService", function($http, $interval, parser) {
           that.storeValues(data);
         };
         
-        var rand1 = Math.random();
-        var rand2 = Math.random();
-        var rand3 = Math.random();
-        var data = [{'symbol':'is_trick', 'instruction': 'assume', 'expression': ['bernoulli', {'type': 'number', 'value': 0.1}], 'directive_id': 1, 'value': rand1},
-                    {'symbol':'weight', 'instruction': 'assume', 'expression': ['if', 'is_trick', ['uniform_continuous', {'type': 'number', 'value': 0.0}, {'type': 'number', 'value': 1.0}], {'type': 'number', 'value': 0.5}], 'directive_id': 2, 'value': rand2},
-                    {'symbol':'w2', 'instruction': 'assume', 'expression': ['bernoulli', {'type': 'number', 'value': 0.1}], 'directive_id': 1, 'value': rand3}
-                ]; //temp
+        var generateRandomData = function() {
+            var rand1 = Math.random();
+            var rand2 = Math.random();
+            var rand3 = Math.random();
+            var data = [{'symbol':'is_trick', 'instruction': 'assume', 'expression': ['bernoulli', {'type': 'number', 'value': 0.1}], 'directive_id': 1, 'value': rand1},
+                        {'symbol':'weight', 'instruction': 'assume', 'expression': ['if', 'is_trick', ['uniform_continuous', {'type': 'number', 'value': 0.0}, {'type': 'number', 'value': 1.0}], {'type': 'number', 'value': 0.5}], 'directive_id': 2, 'value': rand2},
+                        {'symbol':'w2', 'instruction': 'assume', 'expression': ['bernoulli', {'type': 'number', 'value': 0.1}], 'directive_id': 1, 'value': rand3}
+                    ]; //temp
+            return data;
+        };
         
-        //success(data);
+        var useRandomData = true;
         
-        $http.post("http://127.0.0.1:8082/list_directives", [])
-            .success(success)
-            .error(function() {
-                console.log("Could not grab directives");
-            });
+        if (useRandomData) {
+            data = generateRandomData();
+            success(data);
+        } else {
+            $http.post("http://127.0.0.1:8082/list_directives", [])
+                .success(success)
+                .error(function() {
+                    console.log("Could not grab directives");
+                });
+        }
     };
 
     // This function keeps the last 100 values. Can make this dynamic if we want, for now hardcoded to 100
